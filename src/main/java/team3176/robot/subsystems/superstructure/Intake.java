805289a3f1a9815b220d57d1ab304869d539b33a
Jonathan Heidegger <jth3141@gmail.com>
2023-02-01 21:11:12 -0500

    stateless intake example

--- src/main/java/team3176/robot/subsystems/superstructure/Intake.java
+++ src/main/java/team3176/robot/subsystems/superstructure/Intake.java
@@ -5,8 +5,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team3176.robot.constants.Hardwaremap;
 import team3176.robot.constants.SuperStructureConstants;
@@ -14,9 +12,7 @@ import team3176.robot.subsystems.superstructure.Superstructure.GamePiece;
 
 public class Intake extends SubsystemBase {
     private CANSparkMax intake;
-    public enum intakeStates {IDLE,HOLD,INTAKE,SCORE};
     public GamePiece currentGamePiece = GamePiece.NONE;
-    private intakeStates intakeState = intakeStates.IDLE;
     private Intake() {
         intake = new CANSparkMax(Hardwaremap.intake_CID, MotorType.kBrushless);
     }
@@ -28,54 +24,47 @@ public class Intake extends SubsystemBase {
         SmartDashboard.putNumber("intake motor temperature (C)", intake.getMotorTemperature());
     }
 
-    public void setState(intakeStates state) {
-        this.intakeState = state;
+    //states now implemented as functions
+    
+    private void intake() {
+        if(currentGamePiece == GamePiece.CUBE) {
+            setIntakeMotor(SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
+         }
+         else if(currentGamePiece == GamePiece.CONE) {
+             setIntakeMotor(-SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
+         }
     }
+    private void hold() {
+        if(currentGamePiece == GamePiece.CONE) {
+            setIntakeMotor(-SuperStructureConstants.INTAKE_HOLD_POWER,SuperStructureConstants.INTAKE_HOLD_CURRENT_LIMIT_A);
+            
+        } else {
+            setIntakeMotor(-SuperStructureConstants.INTAKE_HOLD_POWER,SuperStructureConstants.INTAKE_HOLD_CURRENT_LIMIT_A);
+        }
+    }
+    private void score() {
+        if(currentGamePiece == GamePiece.CUBE) {
+            setIntakeMotor(-SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
+        }
+        else if(currentGamePiece == GamePiece.CONE) {
+            setIntakeMotor(SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
+        }
+    }
+    private void idle() {
+        setIntakeMotor(0, 0);
+    }
+
     /**
      * to be called with a whileTrue trigger binding
      */
     public Command intakeGamePiece(GamePiece piece) {
-        return this.startEnd(() ->  {setState(intakeStates.INTAKE); this.currentGamePiece = piece;},() -> setState(intakeStates.HOLD));
+        return this.startEnd(() ->  {this.currentGamePiece = piece; intake();},() -> hold());
     }
     /**
      *  scores game piece then returns to idle state
      * @return to be called with a whileTrue trigger binding
      */
-    public Command score() {
-        return this.startEnd(() ->  {setState(intakeStates.INTAKE); this.currentGamePiece = GamePiece.NONE;},() -> setState(intakeStates.IDLE));
-    }
-    
-    @Override
-    public void periodic() {
-        
-        switch(intakeState) {
-            case HOLD:
-                if(currentGamePiece == GamePiece.CONE) {
-                    setIntakeMotor(-SuperStructureConstants.INTAKE_HOLD_POWER,SuperStructureConstants.INTAKE_HOLD_CURRENT_LIMIT_A);
-                    
-                } else {
-                    setIntakeMotor(-SuperStructureConstants.INTAKE_HOLD_POWER,SuperStructureConstants.INTAKE_HOLD_CURRENT_LIMIT_A);
-                }
-                break;
-            case IDLE:
-                setIntakeMotor(0, 0);
-                break;
-            case INTAKE:
-                if(currentGamePiece == GamePiece.CUBE) {
-                   setIntakeMotor(SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
-                }
-                else if(currentGamePiece == GamePiece.CONE) {
-                    setIntakeMotor(-SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
-                }
-                break;
-            case SCORE:
-                if(currentGamePiece == GamePiece.CUBE) {
-                    setIntakeMotor(-SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
-                }
-                else if(currentGamePiece == GamePiece.CONE) {
-                    setIntakeMotor(SuperStructureConstants.INTAKE_OUTPUT_POWER,SuperStructureConstants.INTAKE_CURRENT_LIMIT_A);
-                }
-                break;
-        }
+    public Command scoreGamePiece() {
+        return this.startEnd(() ->  {score(); this.currentGamePiece = GamePiece.NONE;},() -> idle());
     }
 }
