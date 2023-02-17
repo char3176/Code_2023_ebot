4b2ed8a31877f5c412c9a1e71ee1a3a4cddf6829
char@prometheus <char3176@gmail.com>
2023-02-17 18:15:07 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/team3176/robot/constants/Hardwaremap.java
+++ src/main/java/team3176/robot/constants/Hardwaremap.java
@@ -10,6 +10,7 @@ public class Hardwaremap {
     public static int arm_CID = 5;
     public static int claw_CID = 6;
     public static int intake_CID = 7;
+    public static int armCancoder_CID = 8;
 
     /*
      * Drivetrain CIDs
