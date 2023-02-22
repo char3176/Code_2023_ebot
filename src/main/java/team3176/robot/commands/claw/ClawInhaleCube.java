bf80bbdbb82e0c7d9b60e49f36ee426f75e0a7e3
Jonathan Heidegger <jth3141@gmail.com>
2023-02-22 00:53:53 -0500

    claw runs for 0.5 seconds after linebreak is triggered

--- src/main/java/team3176/robot/commands/claw/ClawInhaleCube.java
+++ src/main/java/team3176/robot/commands/claw/ClawInhaleCube.java
@@ -4,6 +4,7 @@
 
 package team3176.robot.commands.claw;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import team3176.robot.subsystems.superstructure.Claw;
 import team3176.robot.subsystems.superstructure.Superstructure.GamePiece;
@@ -11,6 +12,7 @@ import team3176.robot.subsystems.superstructure.Superstructure.GamePiece;
 public class ClawInhaleCube extends CommandBase {
   /** Creates a new ClawInhale. */
   Claw m_Claw = Claw.getInstance();
+  Timer continueRunningTimer = new Timer();
   public ClawInhaleCube() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_Claw);
@@ -27,6 +29,9 @@ public class ClawInhaleCube extends CommandBase {
   public void execute() 
   {
     m_Claw.intake();
+    if(m_Claw.getLinebreakOne()) {
+      continueRunningTimer.restart();
+    }
     System.out.println("ClawInhaleCube");
   }
 
@@ -39,13 +44,6 @@ public class ClawInhaleCube extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (m_Claw.getLinebreakOne() == true)
-    {
-      return false;
-    }
-    else
-    {
-      return true;
-    }
+    return continueRunningTimer.get() > 0.5;
   }
 }
