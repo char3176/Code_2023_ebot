7fe6063cbfc7b2534ade92fea7076f8f6f4786e4
char@yoda <char3176@gmail.com>
2023-03-03 21:20:44 -0500

    Updated arm catch pos and Pod offsets

--- src/main/java/team3176/robot/constants/DrivetrainHardwareMap.java
+++ src/main/java/team3176/robot/constants/DrivetrainHardwareMap.java
@@ -4,13 +4,13 @@ import team3176.robot.constants.SwervePodHardwareID;
 public class DrivetrainHardwareMap {
     //statics constants for swerve pods 
     public static final SwervePodHardwareID pod001 = 
-    new SwervePodHardwareID(  10,  12,  -72.1);
+    new SwervePodHardwareID(  10,  12,  -169.453 );
     public static final SwervePodHardwareID pod002 = 
     new SwervePodHardwareID(  20,  22,  76.3);
     public static final SwervePodHardwareID pod003 = 
-    new SwervePodHardwareID(  30,  32,  -75.9);
+    new SwervePodHardwareID(  30,  32, 47.725);
     public static final SwervePodHardwareID pod004 = 
-    new SwervePodHardwareID( 40,  42,  90.7);
+    new SwervePodHardwareID( 40,  42,  -129.463);
     public static final SwervePodHardwareID pod005 = 
     new SwervePodHardwareID(  13,  14,  97.6);
     public static final SwervePodHardwareID pod006 = 
@@ -18,10 +18,10 @@ public class DrivetrainHardwareMap {
     public static final SwervePodHardwareID pod007 = 
     new SwervePodHardwareID(  33,  34,  -123.2);
     public static final SwervePodHardwareID pod008 = 
-    new SwervePodHardwareID(  43,  44,  -200.676 );
+    new SwervePodHardwareID(  43,  44,  -169.014  );
    
 
-    public static final SwervePodHardwareID FR = pod008;
+    public static final SwervePodHardwareID FR = pod002;
     public static final SwervePodHardwareID FL = pod005;
     public static final SwervePodHardwareID BL = pod006;
     public static final SwervePodHardwareID BR = pod007;
--- src/main/java/team3176/robot/constants/SuperStructureConstants.java
+++ src/main/java/team3176/robot/constants/SuperStructureConstants.java
@@ -45,7 +45,7 @@ public class SuperStructureConstants {
 
   public static final double ARM_ZERO_POS = 6.0;
   public static final double ARM_CARRY_POS = 38 + ARM_ZERO_POS;
-  public static final double ARM_CATCH_POS = 55 + ARM_ZERO_POS;
+  public static final double ARM_CATCH_POS = 53 + ARM_ZERO_POS;
   public static final double ARM_MID_POS = 120 + ARM_ZERO_POS;
   public static final double ARM_HIGH_POS = 175 + ARM_ZERO_POS;
 }
