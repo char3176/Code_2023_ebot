b753ae0a338c0fa02ddcd369252aa9b79f550420
Connor Custer <connor_da_bot@icloud.com>
2023-03-07 20:08:30 -0500

    added switch state machine for balance

--- src/main/java/team3176/robot/commands/drivetrain/AutoBalanceCenter.java
+++ src/main/java/team3176/robot/commands/drivetrain/AutoBalanceCenter.java
@@ -5,11 +5,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import team3176.robot.subsystems.drivetrain.Drivetrain;
 import team3176.robot.subsystems.drivetrain.Drivetrain.driveMode;
-
 public class AutoBalanceCenter extends CommandBase {
     private Drivetrain m_Drivetrain;
-    private boolean isDone = false;
-    private int num_balanced = 0;
+    private enum states {DRIVE,CORRECTING,WAITING,DONE};
+    private states currentState;
+    private double forward = 0.0;
+    private double deadbandDegrees = 8;
+    private Timer correctionTimer = new Timer();
+    private Timer waitTimer = new Timer();
     public AutoBalanceCenter() {
         m_Drivetrain = Drivetrain.getInstance();
         addRequirements(m_Drivetrain);
@@ -18,24 +21,59 @@ public class AutoBalanceCenter extends CommandBase {
     public void initialize() {
         // TODO Auto-generated method stub
         m_Drivetrain.setBrakeMode();
-
+        currentState = states.DRIVE;
     }
     @Override
     public void execute() {
         //double Kp = 0.1;
+        switch(currentState) {
+            case CORRECTING:
+            if(correctionTimer.get() < 0.5){
+                m_Drivetrain.drive(-forward, 0, 0, Drivetrain.coordType.ROBOT_CENTRIC);
+            }  else {
+                currentState = states.WAITING;
+                waitTimer.restart();
+            }
+                break;
+            case DONE:
+                break;
+            case DRIVE:
+            double forward = 0.0;
+        
+            if(m_Drivetrain.getChassisPitch() > 0 + deadbandDegrees) {
+                forward = 0.39;
+            }  else if(m_Drivetrain.getChassisPitch() < 0 - deadbandDegrees) {
+                forward = -0.39;
+            }  else {
+                currentState = states.CORRECTING;
+                correctionTimer.restart();
+            }
+            
+            
+            m_Drivetrain.drive(forward, 0, 0, Drivetrain.coordType.ROBOT_CENTRIC);
+                break;
+            case WAITING:
+                if(waitTimer.get() < 0.5){
+                    m_Drivetrain.drive(0, 0, 0, Drivetrain.coordType.ROBOT_CENTRIC);
+                } else {
+                    if(Math.abs(m_Drivetrain.getChassisPitch()) < deadbandDegrees) {
+                        currentState = states.DONE;
+                    }else {
+                        currentState = states.DRIVE;
+                    }
+                
+                }
 
-        //Bang Bang controller! 
-        double forward = 0.0;
-        double deadbandDegrees = 8;
-        SmartDashboard.putNumber("pitch", m_Drivetrain.getChassisPitch());
-        if(m_Drivetrain.getChassisPitch() > 0 + deadbandDegrees) {
-            forward = 0.42 * Math.pow(.93,num_balanced);
-        } else if(m_Drivetrain.getChassisPitch() < 0 - deadbandDegrees) {
-            forward = -0.42 * Math.pow(.93,num_balanced);
-        } else if(Math.abs(m_Drivetrain.getChassisPitch()) < 2){
-            num_balanced ++;
+                break;
+            default:
+                break;
+
         }
-        m_Drivetrain.drive(forward, 0, 0, Drivetrain.coordType.ROBOT_CENTRIC);
+
+        
+        //Bang Bang controller! 
+        SmartDashboard.putNumber("pitch", m_Drivetrain.getChassisPitch());
+    
     }
     @Override
     public void end(boolean interrupted) {
@@ -43,7 +81,7 @@ public class AutoBalanceCenter extends CommandBase {
     }
     @Override
     public boolean isFinished() {
-        return Timer.getMatchTime() < 0.5 || num_balanced>10;
+        return Timer.getMatchTime() < 0.5 || currentState == states.DONE;
     }
 
 }
