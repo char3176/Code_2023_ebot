700d11d8a41689499fe2e76bf9ff9ba3702b7763
Jonathan Heidegger <jth3141@gmail.com>
2023-03-17 15:51:33 -0400

    added files

---
+++ src/main/java/team3176/robot/commands/drivetrain/FeederPID.java
@@ -0,0 +1,44 @@
+package team3176.robot.commands.drivetrain;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import team3176.robot.subsystems.drivetrain.Drivetrain;
+
+public class FeederPID extends CommandBase{
+    Drivetrain m_Drivetrain;
+    PIDController xController = new PIDController(1.0,0.0,0.0);
+    PIDController yController = new PIDController(1.0,0.0,0.0);;
+    PIDController wController = new PIDController(1.0,0.0,0.0);;
+    double tx = 0.0;
+    double ty = 0.0;
+    double ta = 0.0;
+    NetworkTable vision;
+    public FeederPID() {
+        m_Drivetrain = Drivetrain.getInstance();
+        addRequirements(m_Drivetrain);
+        vision = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+    @Override
+    public void initialize(){
+    }
+    @Override
+    public void execute() {
+        ta = vision.getEntry("ta").getDouble(0.0);
+        ty = vision.getEntry("ty").getDouble(0.0);
+        tx = vision.getEntry("tx").getDouble(0.0);
+        
+        m_Drivetrain.drive(xController.calculate(ta,30),
+                            yController.calculate(tx,0),
+                            0.0);
+    }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    @Override
+    public void end(boolean interrupted) {
+    }
+}
