a9d09d9cb63afc4936186c2b25fb8e0e3c1cf127
Jonathan <jth3141@gmail.com>
2023-04-28 11:59:40 -0400

    building up sim capabilities

--- src/main/java/team3176/robot/Robot.java
+++ src/main/java/team3176/robot/Robot.java
@@ -4,14 +4,19 @@
 
 package team3176.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.inputs.LoggedPowerDistribution;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import team3176.robot.Constants.RobotType;
 import team3176.robot.subsystems.superstructure.Arm;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
@@ -38,9 +43,52 @@ public class Robot extends LoggedRobot{
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     Logger logger = Logger.getInstance();
-    logger.addDataReceiver(new WPILOGWriter("/media/sda2/"));
-    logger.addDataReceiver(new NT4Publisher());
+    logger.recordMetadata("Robot", Constants.getRobot().toString());
+    System.out.println("[Init] Starting AdvantageKit");
+    logger.recordMetadata("RuntimeType", getRuntimeType().toString());
+    logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    switch (BuildConstants.DIRTY) {
+      case 0:
+        logger.recordMetadata("GitDirty", "All changes committed");
+        break;
+      case 1:
+        logger.recordMetadata("GitDirty", "Uncomitted changes");
+        break;
+      default:
+        logger.recordMetadata("GitDirty", "Unknown");
+        break;
+    }
+    switch (Constants.getMode()) {
+      case REAL:
+        String folder = Constants.logFolders.get(Constants.getRobot());
+        if (folder != null) {
+          logger.addDataReceiver(new WPILOGWriter(folder));
+        }
+        logger.addDataReceiver(new NT4Publisher());
+        if (Constants.getRobot() == RobotType.ROBOT_2023C) {
+          LoggedPowerDistribution.getInstance(50, ModuleType.kRev);
+        }
+        break;
+
+      case SIM:
+        logger.addDataReceiver(new WPILOGWriter(""));
+        logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case REPLAY:
+        String path = LogFileUtil.findReplayLog();
+        logger.setReplaySource(new WPILOGReader(path));
+        logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(path, "_sim")));
+        break;
+    }
+
     logger.start();
+
+
     m_robotContainer = new RobotContainer();
     SmartDashboard.putData(CommandScheduler.getInstance());
     m_fisheyeThread = new Thread( () -> {
