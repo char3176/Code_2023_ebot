a83b5aecf43e56579b32e2eeb448a5b45b8143dc
Jonathan <jth3141@gmail.com>
2023-06-12 15:12:02 -0600

    button is being deprecated by wpilib

--- src/main/java/team3176/robot/RobotContainer.java
+++ src/main/java/team3176/robot/RobotContainer.java
@@ -8,12 +8,14 @@ import java.io.File;
 
 
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import team3176.robot.commands.*;
 import team3176.robot.commands.drivetrain.*;
 import team3176.robot.commands.superstructure.claw.ClawIdle;
@@ -41,169 +43,170 @@ import team3176.robot.subsystems.vision.VisionDual;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
-  private final Arm m_Arm;
-  private final Controller m_Controller;
-  private final Claw m_Claw;
-  private final IntakeCube m_IntakeCube;
-  private final IntakeCone m_IntakeCone;
-  private PowerDistribution m_PDH; 
+  private final Arm arm;
+  private final Controller controller;
+  private final Claw claw;
+  private final IntakeCube intakeCube;
+  private final IntakeCone intakeCone;
+  private PowerDistribution pdh; 
 
   
   // is this why we don't have a compressor? private final Compressor m_Compressor
-  private final Drivetrain m_Drivetrain;
-  private final VisionDual m_Vision;
-  private final Superstructure m_Superstructure;
-  private SendableChooser<String> m_autonChooser;
-
+  private final Drivetrain drivetrain;
+  private final VisionDual vision;
+  private final Superstructure superstructure;
+  private SendableChooser<String> autonChooser;
+  
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     // Configure the trigger bindings
-    m_Arm = Arm.getInstance();
-    m_Controller = Controller.getInstance();
-    m_Claw = Claw.getInstance();
-    m_Drivetrain = Drivetrain.getInstance();
-    m_IntakeCube = IntakeCube.getInstance();
-    m_IntakeCone = IntakeCone.getInstance();
-    m_PDH = new PowerDistribution(Hardwaremap.PDH_CID, ModuleType.kRev);
+    arm = Arm.getInstance();
+    controller = Controller.getInstance();
+    claw = Claw.getInstance();
+    drivetrain = Drivetrain.getInstance();
+    intakeCube = IntakeCube.getInstance();
+    intakeCone = IntakeCone.getInstance();
+    pdh = new PowerDistribution(Hardwaremap.PDH_CID, ModuleType.kRev);
 
-    m_Vision = VisionDual.getInstance();
-    m_Superstructure = Superstructure.getInstance();
-    m_Drivetrain.setDefaultCommand(new SwerveDrive(
-        m_Controller::getForward,
-        m_Controller::getStrafe,
-        m_Controller::getSpin));
-    m_Arm.setDefaultCommand(m_Arm.armFineTune( () -> m_Controller.operator.getLeftY()));
-    m_autonChooser = new SendableChooser<>();
+    vision = VisionDual.getInstance();
+    superstructure = Superstructure.getInstance();
+    drivetrain.setDefaultCommand(new SwerveDrive(
+        controller::getForward,
+        controller::getStrafe,
+        controller::getSpin));
+    arm.setDefaultCommand(arm.armFineTune( () -> controller.operator.getLeftY()));
+    autonChooser = new SendableChooser<>();
     File paths = new File(Filesystem.getDeployDirectory(), "pathplanner");
     for (File f : paths.listFiles()) {
       if (!f.isDirectory()) {
         String s = f.getName().split("\\.", 0)[0];
-        m_autonChooser.addOption(s, s);
+        autonChooser.addOption(s, s);
       }
     }
 
   
-    SmartDashboard.putData("Auton Choice", m_autonChooser);
+    SmartDashboard.putData("Auton Choice", autonChooser);
     
     configureBindings();
   }
 
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    m_Controller.getTransStick_Button1().whileTrue(m_Claw.scoreGamePiece());
+    
+    controller.transStick.button(1).whileTrue(claw.scoreGamePiece());
     //m_Controller.getTransStick_Button1().onFalse(new InstantCommand(() -> m_Drivetrain.setTurbo(false), m_Drivetrain));
-    m_Controller.getTransStick_Button2().whileTrue(new IntakeGroundCube());
-    m_Controller.getTransStick_Button2().onFalse(new IntakeRetractSpinot().andThen(m_Superstructure.prepareCarry()));
-    m_Controller.getTransStick_Button2().onFalse(m_Superstructure.prepareCarry());
-    m_Controller.getTransStick_Button3().whileTrue(new SetColorWantState(3));
-    m_Controller.getTransStick_Button3().whileTrue(m_Superstructure.groundCube());
-    m_Controller.getTransStick_Button3().onFalse(new IntakeRetractSpinot());
-    m_Controller.getTransStick_Button3().onFalse(m_Superstructure.prepareCarry());
+    controller.transStick.button(2).whileTrue(new IntakeGroundCube());
+    controller.transStick.button(2).onFalse(new IntakeRetractSpinot().andThen(superstructure.prepareCarry()));
+    controller.transStick.button(2).onFalse(superstructure.prepareCarry());
+    controller.transStick.button(3).whileTrue(new SetColorWantState(3));
+    controller.transStick.button(3).whileTrue(superstructure.groundCube());
+    controller.transStick.button(3).onFalse(new IntakeRetractSpinot());
+    controller.transStick.button(3).onFalse(superstructure.prepareCarry());
 
-    m_Controller.getTransStick_Button4().whileTrue(m_Superstructure.prepareScoreHigh());
-    m_Controller.getTransStick_Button4().onFalse((m_Superstructure.prepareCarry()));
-    m_Controller.getTransStick_Button5().onTrue(new InstantCommand(() -> m_Drivetrain.resetPoseToVision(),m_Drivetrain));
-    m_Controller.getTransStick_Button10().whileTrue(new InstantCommand(() -> m_Drivetrain.setBrakeMode()).andThen(new SwerveDefense()));
+    controller.transStick.button(4).whileTrue(superstructure.prepareScoreHigh());
+    controller.transStick.button(4).onFalse((superstructure.prepareCarry()));
+    controller.transStick.button(5).onTrue(new InstantCommand(drivetrain::resetPoseToVision,drivetrain));
+    controller.transStick.button(10).whileTrue(new InstantCommand(drivetrain::setBrakeMode).andThen(new SwerveDefense()));
      //m_Controller.getTransStick_Button10()
      //    .onFalse(new InstantCommand(() -> m_Drivetrain.setDriveMode(driveMode.DRIVE), m_Drivetrain));
 
     // m_Controller.getRotStick_Button2().whileTrue(new FlipField);
-    m_Controller.getRotStick_Button1().whileTrue(new Turbo(
-      () -> m_Controller.getForward(),
-      () -> m_Controller.getStrafe(),
-      () -> m_Controller.getSpin())
-    );
+    controller.rotStick.button(1).whileTrue(new Turbo(
+      controller::getForward,
+      controller::getStrafe,
+      controller::getSpin
+    ));
  
-    m_Controller.getRotStick_Button2().whileTrue(new SpinLockDrive(
-      () -> m_Controller.getForward(),
-      () -> m_Controller.getStrafe())
+    controller.rotStick.button(2).whileTrue(new SpinLockDrive(
+      controller::getForward,
+      controller::getStrafe)
     ); 
     
 
-    m_Controller.getRotStick_Button3().whileTrue(new InstantCommand(() -> m_Drivetrain.setBrakeMode()).andThen(new SwerveDefense()));
+    controller.rotStick.button(3).whileTrue(new InstantCommand(drivetrain::setBrakeMode).andThen(new SwerveDefense()));
 
-    m_Controller.getRotStick_Button4().whileTrue(m_Superstructure.intakeCubeHumanPlayer());
-    m_Controller.getRotStick_Button4().onFalse(m_Superstructure.prepareCarry());
-    m_Controller.getTransStick_Button8()
-        .whileTrue(new InstantCommand(() -> m_Drivetrain.resetFieldOrientation(), m_Drivetrain));
+    controller.rotStick.button(4).whileTrue(superstructure.intakeCubeHumanPlayer());
+    controller.rotStick.button(4).onFalse(superstructure.prepareCarry());
+    controller.rotStick.button(8)
+        .whileTrue(new InstantCommand(drivetrain::resetFieldOrientation, drivetrain));
 
     double conveyorBumpTime = .1;  //In units of seconds
-    m_Controller.operator.povUp().whileTrue(m_Superstructure.prepareScoreHigh());
-    m_Controller.operator.povUp().onTrue(m_IntakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
-    m_Controller.operator.povRight().whileTrue(m_Superstructure.prepareCarry());
-    m_Controller.operator.povRight().onTrue(m_IntakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
-    m_Controller.operator.povDown().whileTrue(m_Superstructure.prepareCatch());
-    m_Controller.operator.povDown().onTrue(m_IntakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
-    m_Controller.operator.povLeft().whileTrue(m_Superstructure.prepareScoreMid());
-    m_Controller.operator.povLeft().onTrue(m_IntakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
+    controller.operator.povUp().whileTrue(superstructure.prepareScoreHigh());
+    controller.operator.povUp().onTrue(intakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
+    controller.operator.povRight().whileTrue(superstructure.prepareCarry());
+    controller.operator.povRight().onTrue(intakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
+    controller.operator.povDown().whileTrue(superstructure.prepareCatch());
+    controller.operator.povDown().onTrue(intakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
+    controller.operator.povLeft().whileTrue(superstructure.prepareScoreMid());
+    controller.operator.povLeft().onTrue(intakeCube.bumpConveyor().withTimeout(conveyorBumpTime));
 
     // m_Controller.operator.start().onTrue(new ToggleVisionLEDs());
     // m_Controller.operator.back().onTrue(new SwitchToNextVisionPipeline());
 
-    m_Controller.operator.b().onTrue(new SetColorWantState(1));
-    m_Controller.operator.b().whileTrue(m_Superstructure.intakeConeHumanPlayer());
-    m_Controller.operator.b().onFalse(m_Superstructure.prepareCarry());
+    controller.operator.b().onTrue(new SetColorWantState(1));
+    controller.operator.b().whileTrue(superstructure.intakeConeHumanPlayer());
+    controller.operator.b().onFalse(superstructure.prepareCarry());
     
-    m_Controller.operator.x().onTrue(new SetColorWantState(2));
-    m_Controller.operator.x().whileTrue(m_Superstructure.intakeCubeHumanPlayer());
-    m_Controller.operator.x().onFalse(m_Superstructure.prepareCarry());
+    controller.operator.x().onTrue(new SetColorWantState(2));
+    controller.operator.x().whileTrue(superstructure.intakeCubeHumanPlayer());
+    controller.operator.x().onFalse(superstructure.prepareCarry());
 
-    m_Controller.operator.a().onTrue(new SetColorWantState(3));
-    m_Controller.operator.a().whileTrue(m_Superstructure.groundCube());
-    m_Controller.operator.a().onFalse(new IntakeRetractSpinot());
-    m_Controller.operator.a().onFalse(m_Superstructure.prepareCarry());
+    controller.operator.a().onTrue(new SetColorWantState(3));
+    controller.operator.a().whileTrue(superstructure.groundCube());
+    controller.operator.a().onFalse(new IntakeRetractSpinot());
+    controller.operator.a().onFalse(superstructure.prepareCarry());
 
-    m_Controller.operator.y().onTrue(new SetColorWantState(0));
-    m_Controller.operator.y().whileTrue(m_Claw.scoreGamePiece());
-    m_Controller.operator.y().onFalse(new ClawIdle());
+    controller.operator.y().onTrue(new SetColorWantState(0));
+    controller.operator.y().whileTrue(claw.scoreGamePiece());
+    controller.operator.y().onFalse(new ClawIdle());
 
 
-    m_Controller.operator.rightBumper().and(m_Controller.operator.leftBumper().negate()).onTrue(new SetColorWantState(3));
-    m_Controller.operator.rightBumper().and(m_Controller.operator.leftBumper().negate()).whileTrue(new IntakeGroundCube());
-    m_Controller.operator.rightBumper().and(m_Controller.operator.leftBumper().negate()).onFalse(new IntakeRetractSpinot());
+    controller.operator.rightBumper().and(controller.operator.leftBumper().negate()).onTrue(new SetColorWantState(3));
+    controller.operator.rightBumper().and(controller.operator.leftBumper().negate()).whileTrue(new IntakeGroundCube());
+    controller.operator.rightBumper().and(controller.operator.leftBumper().negate()).onFalse(new IntakeRetractSpinot());
     //m_Controller.operator.rightBumper().and(m_Controller.operator.leftBumper().negate()).onFalse(m_Superstructure.prepareCarry());
     
-    m_Controller.operator.leftBumper().and(m_Controller.operator.rightBumper()).whileTrue((new PoopCube()));
+    controller.operator.leftBumper().and(controller.operator.rightBumper()).whileTrue((new PoopCube()));
 //    m_Controller.operator.leftBumper().and(m_Controller.operator.rightBumper()).onFalse(new InstantCommand( () -> m_IntakeCone.idle())); 
     
 
     
-    m_Controller.operator.leftTrigger().onTrue(m_Arm.armSetPositionOnce(140).andThen(m_Arm.armFineTune( () -> m_Controller.operator.getLeftY())));
+    controller.operator.leftTrigger().onTrue(arm.armSetPositionOnce(140).andThen(arm.armFineTune( () -> controller.operator.getLeftY())));
     //m_Controller.operator.leftBumper().onTrue(m_Arm.armSetPositionOnce(200).andThen(m_Arm.armFineTune( () -> m_Controller.operator.getLeftY())));
     //m_Controller.operator.leftBumper().onTrue(new ArmFollowTrajectory(SuperStructureConstants.ARM_MID_POS));
     //m_Controller.operator.start().whileTrue(new OldPoopCube());
-    m_Controller.operator.start().whileTrue(new InstantCommand( () -> m_IntakeCone.spit()));
-    m_Controller.operator.start().onFalse(new InstantCommand( () -> m_IntakeCone.idle()));
+    controller.operator.start().whileTrue(new InstantCommand(intakeCone::spit));
+    controller.operator.start().onFalse(new InstantCommand(intakeCone::idle));
     //m_Controller.operator.start().onFalse(new IntakeRetractSpinot().andThen(m_Superstructure.prepareCarry()));
-    m_Controller.operator.back().whileTrue(m_Superstructure.preparePoop());
+    controller.operator.back().whileTrue(superstructure.preparePoop());
     //m_Controller.operator.leftTrigger().whileTrue(new PoopCube()); 
-    m_Controller.operator.rightTrigger().whileTrue(m_Superstructure.preparePoop());
+    controller.operator.rightTrigger().whileTrue(superstructure.preparePoop());
   }
 
   public void setArmCoast() {
-    m_Arm.setCoastMode();
+    arm.setCoastMode();
   }
   
   public void setArmBrake() {
-    m_Arm.setBrakeMode();
+    arm.setBrakeMode();
   }
   
   public void setThrustCoast() {
-    m_Drivetrain.setCoastMode();
+    drivetrain.setCoastMode();
   }
 
   public void setThrustBrake() {
-    m_Drivetrain.setBrakeMode();
+    drivetrain.setBrakeMode();
   }
 
   public void clearCanFaults(){
-    m_PDH.clearStickyFaults();
+    pdh.clearStickyFaults();
   }
 
   public void printCanFaults(){
-    m_PDH.getStickyFaults();
+    pdh.getStickyFaults();
   }
 
   /**
@@ -213,10 +216,10 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    String chosen = m_autonChooser.getSelected();
+    String chosen = autonChooser.getSelected();
     //String chosen = "wall_cone_exit_balance";
 
-    PathPlannerAuto PPSwerveauto = new PathPlannerAuto(chosen);
-    return PPSwerveauto.getauto();
+    PathPlannerAuto ppSwerveAuto = new PathPlannerAuto(chosen);
+    return ppSwerveAuto.getauto();
   }
 }
